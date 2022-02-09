300e14f55255cb48e6e9068befdaeeeb54d41504
thiago.freitas <thiago.freitas@cepedi.org.br>
2022-02-08 21:17:22 -0300

    Criando Upload de Arquivo

---
+++ SegundoProjeto/src/modules/cars/useCases/importCategory/importCategoryCrontroller.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { ImportCategoryUseCase } from "./importCategoryUseCase";
+
+
+
+
+
+class ImportCategoryController{
+
+    constructor(private importCategoryUseCase: ImportCategoryUseCase){
+
+    }
+
+    handler( request: Request, response: Response): Response{
+        const { file } = request;
+        
+        this.importCategoryUseCase.execute(file);
+
+        return response.send()
+
+    }
+
+}
+
+export { ImportCategoryController }
